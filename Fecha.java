ca5d4b1bf20998c75fe1c05a8ddbc62fc7fc24e4
“SergioNalda” <damj09.2024@gmail.com>
2024-03-13 16:55:01 +0100

    Proyecto documentado

--- Fecha.java
+++ Fecha.java
@@ -14,6 +14,9 @@ public class Fecha {
 	 * @return Esto lo que hace es devolver una fecha completa y lo hace cuando los datos introducidos respetan tanto las condiciones de dias y meses
 	 * como que sean numeros (int) y no letras o palabras (Strings)
 	 * 
+	 * El programa lo que hace es coger los valores de anio,mes y dia que has introducido para validarlos y ver que cumples las condiciones
+	 * que pide para que incluso cuente los meses bisiestos y cuando a validado que los datos no se salen de las condiciones
+	 * te devuelve la fecha final validada.
 	 */
 	public static boolean validarFecha(int anio, int mes, int dia) {
 		fechaValida = false;
